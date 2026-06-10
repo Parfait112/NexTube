b6d43533cce5a1b35c1d3704d6114ad4bbc514eb
parfait <parfaittchambe589@gmail.com>
2026-06-10 15:54:44 +0100

    push changes to the database

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-
+import { homeLayout as HomeLayout } from "@/modules/home/ui/layout/home-layout";
 
 interface LayoutProps{
     children: React.ReactNode;
@@ -7,7 +7,9 @@ interface LayoutProps{
 function layout({children}: LayoutProps) {
   return (
     <div>
+      <HomeLayout>
         {children}  
+      </HomeLayout>
     </div>
   )
 }
---
+++ src/modules/home/home.tsx
@@ -0,0 +1,16 @@
+
+import { HydrateClient , trpc } from "@/trpc/server"
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+
+function Home() {
+  
+
+  return (
+    <div >
+        
+    </div>
+  )
+}
+
+export default Home 
