901241b7d785b07813bb5e15ec43363b6f295ac1
parfait <parfaittchambe589@gmail.com>
2026-06-11 17:01:40 +0100

    category

--- src/components/filter-carousel.tsx
+++ src/components/filter-carousel.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { Badge } from "./ui/badge";
-import { Carousel,CarouselApi, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
 
-interface FilterCarouselProps{
+interface FilterCarouselProps {
     value?: string | null;
     isLoading?: boolean;
     onSelect?: (value: string | null) => void;
@@ -13,31 +13,38 @@ interface FilterCarouselProps{
     }[];
 }
 
-export const FilterCarousel = ({value, isLoading, onSelect, data}: FilterCarouselProps) =>{
-    return(
-        <div className="relatiive w-full">
+export const FilterCarousel = ({ value, isLoading, onSelect, data }: FilterCarouselProps) => {
+    return (
+        <div className="relative w-full">
             <Carousel
-            opts={{
-                align: "start",
-                dragFree: true,
-            }}
-            className="w-full px-12">
+                opts={{ align: "start", dragFree: true }}
+                className="w-full px-12"
+            >
                 <CarouselContent className="-ml-3">
-                    <CarouselItem>
-                        <Badge variant={value == null ? "default" :"secondary"} className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm">
+                    <CarouselItem className="pl-3 basis-auto">
+                        <Badge
+                            onClick={() => onSelect?.(null)}
+                            variant={value == null ? "default" : "secondary"}
+                            className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+                        >
                             All
                         </Badge>
                     </CarouselItem>
-                    {!isLoading && data.map( (item) =>(
-                        <CarouselItem key={item.value} className="pl-3 basic-auto">
-                            <Badge variant={value == item.value ? "default" : "secondary"}>
+                    {!isLoading && data.map((item) => (
+                        <CarouselItem key={item.value} className="pl-3 basis-auto">
+                            <Badge
+                                onClick={() => onSelect?.(item.value)}
+                                variant={value === item.value ? "default" : "secondary"}
+                                className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+                            >
                                 {item.label}
                             </Badge>
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-
+                <CarouselPrevious className="left-0 z-20" />
+                <CarouselNext className="right-0 z-20" />
             </Carousel>
         </div>
-    )
-}
+    );
+};
--- src/modules/home/views/home-view.tsx
+++ src/modules/home/views/home-view.tsx
@@ -7,7 +7,7 @@ interface HomeViewProps{
 
 export const HomeView = ({categoryId}: HomeViewProps) =>{
   return (
-    <div className="max-w-[200] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6 ">  
+    <div className="w-full mb-10 px-4 pt-2.5 flex flex-col gap-y-6 ">  
         <CategoriesSection categoryId={categoryId} />
     </div>
   )  
