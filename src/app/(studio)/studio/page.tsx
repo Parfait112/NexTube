3ee62fefe7e114f005112d7fd4e79f699212b62e
parfait <parfaittchambe589@gmail.com>
2026-07-21 17:13:16 +0100

    mux

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,9 +1,11 @@
+export const dynamic = 'force-dynamic';
+
 import { DEFAULT_LIMIT } from "@/constants";
 import { StudioView } from "@/modules/studio/ui/view/studio-view";
 import { HydrateClient, trpc } from "@/trpc/server"
 
 const Page = async () =>{
-    void trpc.studio.getMany.prefetchInfinite({
+    await trpc.studio.getMany.prefetchInfinite({
         limit: DEFAULT_LIMIT,
     });
 
